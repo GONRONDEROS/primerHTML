8b8b45de84536461fec014fa718c96431741bb2a
Gonzalo Ronderos <gonzalorond@gmail.com>
2022-02-10 18:19:24 -0300

    Se cambio nombre de html de index a home y carpeta img a imagenes. se hicieron los cambios corrrespondientes en el progrma

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
+const app = express();
+const publicPath = path.resolve(__dirname, "./public") ;
+app.use(express.static(publicPath));
+
+app.listen(5000, () => {
+    console.log("Servidor funcionando correctamente en 3000");
+});
+
+app.get("/home", (req, res) => {
+    const fileToSend = path.join(__dirname, "views/home.html");
+    res.sendFile(fileToSend);
+});
+
+
+
+
